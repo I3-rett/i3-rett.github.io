61cdeb1e6a8d2a7a88782c5375fc1b51f5ccee64
Brett <uselessbrett@gmail.com>
2025-05-22 14:30:24 +0200

    Revamped whole website

---
+++ src/app/page.tsx
@@ -0,0 +1,99 @@
+import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Card } from '@/components/Card'
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  LinkedInIcon,
+} from '@/components/SocialIcons'
+import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
+import { formatDate } from '@/lib/formatDate'
+import { Article } from './articles/page'
+
+function SocialLink({
+  icon: Icon,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Link> & {
+  icon: React.ComponentType<{ className?: string }>
+}) {
+  return (
+    <Link className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    </Link>
+  )
+}
+
+function Photos() {
+  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  const photos = [] as StaticImageData[]
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        {photos.map((image, imageIndex) => (
+          <div
+            key={image.src}
+            className={clsx(
+              'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+              rotations[imageIndex % rotations.length],
+            )}
+          >
+            <Image
+              src={image}
+              alt=""
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default async function Home() {
+  let articles = (await getAllArticles()).slice(0, 4)
+
+  return (
+    <>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            Software engineer for a living, many other things for fun.
+          </h1>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            I’m Brett, a software engineer based in France. I work with the Javascript ecosystem (from JS to NextJS).
+            I’m passionate about learning things, from new IT stuff to game dev. I also enjoy writing about my experiences and sharing
+            my knowledge with others. In my free time, I mainly play strategy-like video games.
+          </p>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://github.com/I3-rett"
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+            />
+            <SocialLink
+              href="https://www.linkedin.com/in/brett-choquet/"
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
+            />
+          </div>
+        </div>
+      </Container>
+      <Photos />
+      <Container className="mt-8">
+        <div className="max-w-2xl">
+          <h2 className="text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
+            Latest articles
+          </h2>
+          <div className="mt-6 space-y-16 divide-y divide-zinc-200 dark:divide-zinc-700">
+            {articles.slice(0, 5).map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
